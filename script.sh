328411da340371ca7b323bde8dfc00659e19f3d6
Seppe Gijsels <seppe.gijsels@gmail.com>
2024-09-28 09:44:04 +0200

    oplossen bugs

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 echo "Dit is een shell script"
-echo " Dit is een extra toegevoegde regel nadat we dit gepulled hebben online"
+echo "Dit is een extra toegevoegde regel nadat we dit gepulled hebben online"
+echo "Dit is van de Bug Fix"
